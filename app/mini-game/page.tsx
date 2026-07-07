918293af1fa7976bcbdec8b3c97fcd0683d0db29
taka <taka@gmail.com>
2026-07-08 08:19:26 +0900

    release

---
+++ app/mini-game/page.tsx
@@ -0,0 +1,14 @@
+export default function MiniGamePage() {
+  return (
+    <main
+      style={{
+        background: "white",
+        minHeight: "100vh",
+        color: "black",
+        padding: "40px",
+      }}
+    >
+      <h1>ミニゲーム</h1>
+    </main>
+  );
+}
