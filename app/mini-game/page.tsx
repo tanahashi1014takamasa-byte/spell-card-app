ba611f88241b31c5667838f72b2fbfc4526925ee
taka <taka@gmail.com>
2026-07-08 09:06:50 +0900

    release

--- app/mini-game/page.tsx
+++ app/mini-game/page.tsx
@@ -16,45 +16,54 @@ export default function MiniGamePage() {
         paddingTop: "100px",
       }}
     >
-      <h1>🎆 夏祭りミニゲーム</h1>
+      {isEntered ? (
+        <div>
+          <h1>🎆 夏祭りミニゲーム屋</h1>
 
-      <p>
-        獲得チップ数を入力してください
-      </p>
+          <p>
+            🧔「へい、らっしゃい！」
+          </p>
 
-      <input
-        type="number"
-        value={chips}
-        onChange={(e) => setChips(e.target.value)}
-        style={{
-          fontSize: "24px",
-          width: "120px",
-          textAlign: "center",
-        }}
-      />
+          <p>
+            所持チップ：{chips}枚
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h1>🎆 夏祭りミニゲーム</h1>
 
-      <span style={{ fontSize: "24px" }}>
-        枚
-      </span>
+          <p>
+            獲得チップ数を入力してください
+          </p>
 
-      <br /><br />
+          <input
+            type="number"
+            value={chips}
+            onChange={(e) => setChips(e.target.value)}
+            style={{
+              fontSize: "24px",
+              width: "120px",
+              textAlign: "center",
+            }}
+          />
 
-      <button
-  onClick={() => setIsEntered(true)}
-  style={{
-    fontSize: "24px",
-    padding: "10px 30px",
-  }}
->
-  入場する
-</button>
+          <span style={{ fontSize: "24px" }}>
+            枚
+          </span>
 
-{isEntered && (
-  <p>
-    入場しました！
-  </p>
-)}
+          <br /><br />
 
+          <button
+            onClick={() => setIsEntered(true)}
+            style={{
+              fontSize: "24px",
+              padding: "10px 30px",
+            }}
+          >
+            入場する
+          </button>
+        </div>
+      )}
     </main>
   );
 }
