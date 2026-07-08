7b9700d154b5c6cd6f010b1778c5198c0f9c3292
taka <taka@gmail.com>
2026-07-09 08:42:47 +0900

    release

--- app/mini-game/page.tsx
+++ app/mini-game/page.tsx
@@ -3,9 +3,7 @@
 import { useState } from "react";
 
 export default function MiniGamePage() {
-  const [chips, setChips] = useState("");
-  const [isEntered, setIsEntered] = useState(false);
-  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [started, setStarted] = useState(false);
 
   return (
     <main
@@ -13,97 +11,46 @@ export default function MiniGamePage() {
         minHeight: "100vh",
         background: "#111",
         color: "white",
-        textAlign: "center",
-        paddingTop: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "monospace",
       }}
     >
-      {isEntered ? (
-  isGameStarted ? (
-    <div>
-      <h1>おばはんのファミコンワールド</h1>
-    </div>
-  ) : (
-    <div>
-
-          <img
-      src="/images/nishida.png"
-      alt="西田店長"
-      style={{
-        width: "200px",
-        marginTop: "20px",
-      }}
-    />
-
-          <p>
-            おーおーよーきたな
-            どうせ暇やろお前？ちょっと遊んでいけや
-            お？いやこのページ見つける時点で暇やんか
-            あ？ええからスッとやれや！！
-            やれ！！！
-            や！！れ！！！
-            やれ言うとんねんボケが
-            もうあれやな
-            一発でクリアできんかったらお前の鼻骨を折る
-          </p>
-
-          <p>
-            所持チップ：{chips}枚
-          </p>
-
-
-          <button
-  onClick={() => setIsGameStarted(true)}
-  style={{
-    fontSize: "24px",
-    padding: "10px 30px",
-    marginTop: "20px",
-  }}
->
-  ゲーム開始
-</button>
-        </div>
-
-        
-)
-      ) : (
-        <div>
-          <h1>🎆 夏祭りミニゲーム</h1>
-
-          <p>
-            獲得チップ数を入力してください
-          </p>
-
-          <input
-            type="number"
-            value={chips}
-            onChange={(e) => setChips(e.target.value)}
+      {!started ? (
+        <>
+          <h1
             style={{
-              fontSize: "24px",
-              width: "120px",
-              textAlign: "center",
-            }}
-          />
-
-          <span style={{ fontSize: "24px" }}>
-            枚
-          </span>
-
-          <br />
-
-
-
-          <br /><br />
-
-          <button
-            onClick={() => setIsEntered(true)}
-            style={{
-              fontSize: "24px",
-              padding: "10px 30px",
+              fontSize: "40px",
+              marginBottom: "50px",
             }}
           >
-            入場する
+            EXTRA INNING
+          </h1>
+
+          <button
+            onClick={() => setStarted(true)}
+            style={{
+              fontSize: "25px",
+              padding: "15px 40px",
+              cursor: "pointer",
+              fontFamily: "monospace",
+            }}
+          >
+            ▶ PLAY
           </button>
-        </div>
+        </>
+      ) : (
+        <>
+          <h1>EXTRA INNING</h1>
+
+          <p>GAME START</p>
+
+          <p>
+            所持金：0円
+          </p>
+        </>
       )}
     </main>
   );
