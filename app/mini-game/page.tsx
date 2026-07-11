45ba8e6ced2f8da0c250a4e728df92ae3b8c56db
taka <taka@gmail.com>
2026-07-11 17:18:37 +0900

    release

--- app/mini-game/page.tsx
+++ app/mini-game/page.tsx
@@ -255,7 +255,7 @@ useEffect(() => {
   <div
     style={{
       position: "fixed",
-      left: "50%",
+      right: "10%",
       bottom: "50%",
       transform: "translate(-50%, -50%)",
       background: "black",
