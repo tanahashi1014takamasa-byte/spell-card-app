7b82d37b952ef51d82167b0c65518d8043e759ff
taka <taka@gmail.com>
2026-07-13 14:41:33 +0900

    release

--- app/page.tsx
+++ app/page.tsx
@@ -534,13 +534,17 @@ return (
 />
 ) : (
   <img
-    src={zoomImage}
-    style={{
-      width: "85vw",
-      maxWidth: "500px",
-      borderRadius: "12px",
-    }}
-  />
+  src={zoomImage}
+  style={{
+    width: "85vw",
+    maxWidth: "500px",
+    borderRadius: "12px",
+    animation:
+      zoomImage === "/cards/四次元マンション.png"
+        ? "rareFlash 0.8s infinite"
+        : "none",
+  }}
+/>
 )}
   </div>
 )}
